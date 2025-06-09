d828e3e62c44cef828d9187e75ee25c78181efe3
Lucas de Souza Santos Viana <lucas.ssv.dev@gmail.com>
2025-06-09 11:10:20 -0300

    Mudança nos Endpoints

--- apiclaro3/src/main/java/com/example/apiclaro/web/TaskController.java
+++ apiclaro3/src/main/java/com/example/apiclaro/web/TaskController.java
@@ -11,6 +11,7 @@ import java.util.UUID;
 @RestController
 @RequestMapping("task")
 public class TaskController {
+
     private final TaskService service;
 
     public TaskController(TaskService service) {
@@ -18,49 +19,49 @@ public class TaskController {
     }
 
 
+
+    //Requisições HTTP:
+
     //Post Mappings
-    @PostMapping("new-task")
+    @PostMapping
     public void createTask(@RequestBody TaskDetails task){
         service.createTask(task);
     }
 
 
     //Get Mappings
-    @GetMapping("all")
+    @GetMapping
     public List<TaskOutput> getTasks(){
         return service.getTasks();
     }
 
-    @GetMapping("find/{id}")
+    @GetMapping("{id}")
     public TaskOutput getTaskById(@PathVariable("id") UUID id){
         return service.getTaskById(id);
     }
 
-//    @GetMapping("find-completed")
-//    public List<TaskOutput> getTaskByCompleted(){ return service.getTaskByCompleted(); }
-
 
     //Delete Mappings
-    @DeleteMapping("delete-all")
+    @DeleteMapping
     public void deleteAll(){
         service.deleteAll();
     }
 
-    @DeleteMapping("delete/{id}")
+    @DeleteMapping("{id}")
     public void deleteById(@PathVariable("id") UUID id){
         service.deleteById(id);
     }
 
 
     //Patch Mappings
-    @PatchMapping("status/{id}") //ESSE AQUI É PRA ALTERAR A CONCLUSÃO SEM ABRIR A TASK. TRANSFERIR ELA PRA AREA DE CONCLUÍDO.
+    @PatchMapping("{id}/status") //ESSE AQUI É PRA ALTERAR A CONCLUSÃO SEM ABRIR A TASK. TRANSFERIR ELA PRA AREA DE CONCLUÍDO.
     public TaskOutput editTaskStatus(@PathVariable UUID id){
         return service.editTaskStatus(id);
     }
 
 
     //Put Mappings
-    @PutMapping("save/{id}") //TODA VEZ QUE ALGUEM ABRIR E ALTERAR QUALQUER CONTEÚDO VAI SER LANÇADO ESSE AQUI.
+    @PutMapping("{id}") //TODA VEZ QUE ALGUEM ABRIR E ALTERAR QUALQUER CONTEÚDO VAI SER LANÇADO ESSE AQUI.
     public TaskOutput editTask(@PathVariable("id") UUID id,
                                @RequestBody TaskDetails task){
         return service.editTask(id, task);
