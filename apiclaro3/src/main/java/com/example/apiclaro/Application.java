03803f567fa99497624cabfb4e4f655b6a0b9705
Lucas de Souza Santos Viana <lucas.ssv.dev@gmail.com>
2025-06-16 14:01:13 -0300

    ♻️Renamed: Apiclaro3Application renomeado pra apenas Application. Teste e YML também renomeados.

--- apiclaro3/src/main/java/com/example/apiclaro/Application.java
+++ apiclaro3/src/main/java/com/example/apiclaro/Application.java
@@ -5,9 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Apiclaro3Application {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Apiclaro3Application.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
